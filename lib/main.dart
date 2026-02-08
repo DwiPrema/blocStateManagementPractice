4e7222cdc63db6e2630de992012a0d124689e364
DwiPrema <madedwiprema08@gmail.com>
2026-02-08 18:59:09 +0800

    modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_practice/bloc_provider/bloc.dart';
+import 'package:flutter_bloc_practice/bloc_provider/home.dart';
+import 'package:flutter_bloc_practice/color_state/color_home.dart';
 import 'package:flutter_bloc_practice/counter_cubit_learn/counter_home.dart';
+import 'package:flutter_bloc_practice/counter_state_bloc_consumer/counter_home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,8 +15,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: CounterHome(),
+    //USE BLOC PROVIDER
+    return BlocProvider(
+      create: (context) => Counter(),
+      child: MaterialApp(home: Home()),
     );
+
+    //USE CUBIT
+    // return MaterialApp(home: CounterCubitHome());
+
+    //USE BLOC CONSUMER
+    // return MaterialApp(home: CounterBlocConsumer());
+
+    //USE CUBIT FOR COLORHOME PAGE
+    // return MaterialApp(home: ColorHome());
   }
 }
